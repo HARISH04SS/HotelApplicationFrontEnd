c1c0d184b5d376884eb67a826af4bb840d4e5616
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-16 00:34:51 +0530

    basic home screen and roter setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomeNav from './wrappers/HomeNav';
+import Register from './components/Register';
+import Login from './components/Login';
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomeNav />,
+    children:[
+      {
+        path:"register",
+        element:<Register />
+      },
+      {
+        path:"login",
+        element:<Login />
+      }
+    ]
+  }
+]);
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <RouterProvider router={router}/>;
+};
 
-export default App
+export default App;
